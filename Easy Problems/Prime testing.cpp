32ffc62996a4cdbb21015a6769e0e7de89cb11d3
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-02-10 02:10:40 +0600

    Update Prime testing.cpp

--- Easy Problems/Prime testing.cpp
+++ Easy Problems/Prime testing.cpp
@@ -5,7 +5,8 @@ using namespace std;
 bool isPrime(int n)
     {
         if(n<2) return false;
-        for(int i=2;i<=sqrt(n);i++)
+        int limit=sqrt(n);
+        for(int i=2;i<=limit;i++)
         {
             if(n%i==0) return false;
         }
