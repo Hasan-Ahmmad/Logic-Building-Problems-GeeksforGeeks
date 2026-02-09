d919385a02fe0c4068b6f110fe6aa5953b731e08
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-02-10 01:04:17 +0600

    Create Dice Problem.cpp

---
+++ Basic Problems/Dice Problem.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n; cin>>n;
+    cout<<7-n<<endl;
+    return 0;
+}
